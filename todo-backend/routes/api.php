15235c84ac22fef7dff3b4a4b05a7b92a46e2e14
hgrafa <hugorafell@gmail.com>
2024-04-21 00:50:42 -0300

    fix: adjust route prefix

--- todo-backend/routes/api.php
+++ todo-backend/routes/api.php
@@ -2,25 +2,28 @@
 
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\TodoController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user/data', [AuthController::class, 'getUser']);
-});
+Route::group(
+    [
+        'prefix' => 'v1',
+        'namespace' => 'App\Http\Controllers\Api\V1'
+    ],
+    function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+    }
+);
 
 Route::group(
-    ['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'],
+    [
+        'prefix' => 'v1',
+        'namespace' => 'App\Http\Controllers\Api\V1',
+        'middleware' => 'auth:sanctum'
+    ],
     function () {
         Route::apiResource('todos', TodoController::class);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/user/data', [AuthController::class, 'getUser']);
     }
 );
