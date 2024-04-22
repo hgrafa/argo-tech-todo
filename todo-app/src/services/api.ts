f9bbd936c48722cb3ba5aae267759d24ec49a524
hgrafa <hugorafell@gmail.com>
2024-04-22 00:04:57 -0300

    fix: updating backend url

--- todo-app/src/services/api.ts
+++ todo-app/src/services/api.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-// TODO: change to .env file
-const BACKEND_URL = 'http://localhost:8000';
+const BACKEND_URL = 'https://argo-tech-todo-1.onrender.com/api';
 
 export const api = axios.create({
-  baseURL: `${BACKEND_URL}/api`,
+  baseURL: BACKEND_URL,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
