12c30af60fb6527f9f526609ad365f01095157b6
hgrafa <hugorafell@gmail.com>
2024-04-22 00:02:33 -0300

    fix: fixes loading spinner import

--- todo-app/src/components/LoadingSpinner.tsx
+++ todo-app/src/components/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 import { Center, Spinner } from "@gluestack-ui/themed";
 
-export function Loading() {
+export function LoadingSpinner() {
   return (
     <Center flex={1} bg="$backgroundDark900">
       <Spinner color="$indigo600" size="large" />
